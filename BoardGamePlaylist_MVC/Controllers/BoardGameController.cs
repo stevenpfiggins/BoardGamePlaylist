b2b10b9157c30bbadccd601a66e8a33de36be761
stevenpfiggins <sfiggins43@gmail.com>
2018-11-20 14:51:33 -0500

    Initial Commit

---
+++ BoardGamePlaylist_MVC/Controllers/BoardGameController.cs
@@ -0,0 +1,120 @@
+﻿using BoardGamePlaylist_MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BoardGamePlaylist_MVC.Controllers
+{
+    public class BoardGameController : Controller
+    {
+        private BoardGameDBContext db = new BoardGameDBContext();
+        // GET: BoardGame
+        public ActionResult Index()
+        {
+            return View(db.BoardGames.ToList());
+        }
+
+        //GET: BoardGame/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //POST: BoardGame/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "BoardGameID,Name,MainMechanism,Description,Weight,Rating")] BoardGame boardGame)
+        {
+            if (ModelState.IsValid)
+            {
+                db.BoardGames.Add(boardGame);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(boardGame);
+        }
+
+        //GET: BoardGame/Delete/{id}
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BoardGame boardGame = db.BoardGames.Find(id);
+            if (boardGame == null)
+            {
+                return HttpNotFound();
+            }
+            return View(boardGame);
+        }
+
+        //POST: BoardGame/Delete/{id}
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            BoardGame boardGame = db.BoardGames.Find(id);
+            db.BoardGames.Remove(boardGame);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        //GET: BoardGame/Edit/{id}
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BoardGame boardGame = db.BoardGames.Find(id);
+            if (boardGame == null)
+            {
+                return HttpNotFound();
+            }
+            return View(boardGame);
+        }
+
+        //POST: BoardGame/Edit/{id}
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include ="BoardGameID,Name,MainMechanism,Description,Weight,Rating")] BoardGame boardGame)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(boardGame).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(boardGame);
+        }
+
+        //GET: BoardGame/Details/{id}
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BoardGame boardGame = db.BoardGames.Find(id);
+            if (boardGame == null)
+            {
+                return HttpNotFound();
+            }
+            return View(boardGame);
+        }
+    }
+}
---
+++ BoardGamePlaylist_MVC/Models/BoardGame.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace BoardGamePlaylist_MVC.Models
+{
+    public class BoardGame
+    {
+        [Key]
+        public int BoardGameID { get; set; }
+        public string Name { get; set; }
+        public string MainMechanism { get; set; }
+        public string Description { get; set; }
+        public float Weight { get; set; }
+        public float Rating { get; set; }
+    }
+
+    public class BoardGameDBContext : DbContext
+    {
+        public DbSet<BoardGame> BoardGames { get; set; }
+    }
+}
